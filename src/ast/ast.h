61cf1ae6b613a021c57bcb65ad713e0b66bf62a3
Jeff613 <143241899+Jeffwngl@users.noreply.github.com>
2026-06-04 23:23:49 +1000

    testing ast node organization

--- src/ast/ast.h
+++ src/ast/ast.h
@@ -7,28 +7,61 @@
 
 #include "../lexer/lexer.h"
 
+// TODO: Move this to a generator to generate ast.h, metaprogram the trees
+
+/**
+ * AST nodes here only store the data of the node, no operations, operations
+ * are defined separately as functions which take in a node to avoid the
+ * expression problem.
+ */
+
+struct Expr;
+struct Stmt;
+
+struct LiteralExpr;
+struct IdentifierExpr;
+struct BinaryExpr;
+struct UnaryExpr;
+struct PostFixExpr;
+
 typedef std::unique_ptr<Expr> ExprPtr;
 typedef std::unique_ptr<Stmt> StmtPtr;
+
+struct ExprVisitor
+{
+    // overriden by class which implements the visit function, e.g. print,
+    // compile
+    virtual std::string visitLiteralExpr(const LiteralExpr& expr) = 0;
+    virtual std::string visitBinaryExpr(const BinaryExpr& expr) = 0;
+    virtual std::string visitUnaryExpr(const UnaryExpr& expr) = 0;
+    virtual std::string visitIdentifierExpr(const IdentifierExpr& expr) = 0;
+    virtual std::string visitPostFixExpr(const PostFixExpr& expr) = 0;
+
+    virtual ~ExprVisitor() = default;
+};
 
 /**
  * Define destructors through base class using virtual destructor
  */
 
-struct ASTNode
+struct AstNode
 {
-    ~ASTNode() = default;
+    virtual ~AstNode() = default;
 };
 
 // expression: produces values
-struct Expr : ASTNode
+struct Expr : AstNode
 {
-    ~Expr() = default;
+    // pure virtual functions, all concrete functions must implement
+    // overridden by all expression implementations, e.g. literal expr
+    virtual std::string accept(ExprVisitor& visitor) const = 0;
+    virtual ~Expr() = default;
 };
 
 // statement: performs actions
-struct Stmt : ASTNode
+struct Stmt : AstNode
 {
-    ~Stmt() = default;
+    virtual ~Stmt() = default;
 };
 
 // raw value written in code, e.g. "this is a string"
@@ -36,6 +69,12 @@ struct LiteralExpr : Expr
 {
     Token value;
     explicit LiteralExpr(Token token) : value(std::move(token)) {}
+
+    std::string accept(ExprVisitor& visitor) const override
+    {
+        // passes in the current LiteralExpr object
+        return visitor.visitLiteralExpr(*this);
+    }
 };
 // explicit stops silent conversions, we have to convert it ourselves
 // move avoids creating copy when possible, also needed for unique_ptr
@@ -45,6 +84,14 @@ struct IdentifierExpr : Expr
 {
     Token name;
     explicit IdentifierExpr(Token token) : name(std::move(token)) {}
+
+    std::string accept(ExprVisitor& visitor) const override
+    {
+        // passes in the current object into the defined function, e.g. print
+        // print implements visitIdentifierExpr and accept returns the result
+        // of prints implementation of visitIdentifierExpr
+        return visitor.visitIdentifierExpr(*this);
+    }
 };
 
 // expression with two sides and an operator, e.g. 4 + 2
@@ -57,6 +104,11 @@ struct BinaryExpr : Expr
         : op(std::move(token)), l(std::move(left)), r(std::move(right))
     {
     }
+
+    std::string accept(ExprVisitor& visitor) const override
+    {
+        return visitor.visitBinaryExpr(*this);
+    }
 };
 
 // expression with one operand and operator, e.g. --x or -(x + 4)
@@ -68,6 +120,11 @@ struct UnaryExpr : Expr
         : op(std::move(token)), r(std::move(right))
     {
     }
+
+    std::string accept(ExprVisitor& visitor) const override
+    {
+        return visitor.visitUnaryExpr(*this);
+    }
 };
 
 // e.g. x++, x--;
@@ -78,6 +135,11 @@ struct PostFixExpr : Expr
     explicit PostFixExpr(Token token, ExprPtr left)
         : op(std::move(token)), l(std::move(left))
     {
+    }
+
+    std::string accept(ExprVisitor& visitor) const override
+    {
+        return visitor.visitPostFixExpr(*this);
     }
 };
 
@@ -127,11 +189,32 @@ struct ForStmt : Stmt
     }
 };
 
-class AST
+// TODO: See if using dynamic cast might be better than creating new struct for
+// every return type
+class AstPrinter : public ExprVisitor
 {
 public:
+    std::string print(const Expr& expr)
+    {
+        // implement dynamic cast here
+        return expr.accept(*this);
+    }
+
+    std::string visitLiteralExpr(const LiteralExpr& expr) override
+    {
+        return expr.value.val;
+    }
+
+    std::string visitIdentifierExpr(const IdentifierExpr& expr) override
+    {
+        return expr.name.val;
+    }
+
 private:
-    ASTNode root;
+    std::string parenthesize(const std::string& name, const Expr& expr)
+    {
+        return '(' + name + ' ' + print(expr) + ')';
+    }
 };
 
 #endif
