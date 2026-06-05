705f713b376203eef442eb78ec53b040d50dd5d7
Jeff613 <143241899+Jeffwngl@users.noreply.github.com>
2026-06-05 17:52:18 +1000

    added debugging tools

---
+++ src/utils/print_ast.h
@@ -0,0 +1,186 @@
+#include "../ast/ast.h"
+#include "../lexer/lexer.h"
+#include "../parser/parser.h"
+#include <iostream>
+
+void printExpr(const Expr* expr);
+void printStmt(const Stmt* stmt);
+
+void printExpr(const Expr* expr)
+{
+    if (expr == nullptr)
+    {
+        std::cout << "null";
+        return;
+    }
+
+    if (const auto* primary = dynamic_cast<const PrimaryExpr*>(expr))
+    {
+        std::cout << "Primary(" << primary->value.val << ")";
+    }
+    else if (const auto* ident = dynamic_cast<const IdentifierExpr*>(expr))
+    {
+        std::cout << "Identifier(" << ident->name.val << ")";
+    }
+    else if (const auto* binary = dynamic_cast<const BinaryExpr*>(expr))
+    {
+        std::cout << "Binary(";
+        printExpr(binary->l.get());
+        std::cout << " " << binary->op.val << " ";
+        printExpr(binary->r.get());
+        std::cout << ")";
+    }
+    else if (const auto* unary = dynamic_cast<const UnaryExpr*>(expr))
+    {
+        std::cout << "Unary(" << unary->op.val << " ";
+        printExpr(unary->r.get());
+        std::cout << ")";
+    }
+    else if (const auto* postfix = dynamic_cast<const PostFixExpr*>(expr))
+    {
+        std::cout << "Postfix(";
+        printExpr(postfix->l.get());
+        std::cout << postfix->op.val << ")";
+    }
+    else if (const auto* call = dynamic_cast<const CallExpr*>(expr))
+    {
+        std::cout << "Call(";
+
+        printExpr(call->called.get());
+
+        std::cout << "(";
+
+        for (size_t i = 0; i < call->args.size(); i++)
+        {
+            if (i > 0)
+                std::cout << ", ";
+
+            printExpr(call->args[i].get());
+        }
+
+        std::cout << "))";
+    }
+    else
+    {
+        std::cout << "UnknownExpr";
+    }
+}
+
+void printStmt(const Stmt* stmt)
+{
+    if (stmt == nullptr)
+    {
+        std::cout << "null";
+        return;
+    }
+
+    if (const auto* var = dynamic_cast<const VarDecStmt*>(stmt))
+    {
+        std::cout << "VarDec(" << var->name.val;
+
+        if (var->value)
+        {
+            std::cout << " = ";
+            printExpr(var->value.get());
+        }
+
+        std::cout << ")";
+    }
+    else if (const auto* exprStmt = dynamic_cast<const ExprStmt*>(stmt))
+    {
+        std::cout << "ExprStmt(";
+        printExpr(exprStmt->expr.get());
+        std::cout << ")";
+    }
+    else if (const auto* ifStmt = dynamic_cast<const IfStmt*>(stmt))
+    {
+        std::cout << "If(";
+        printExpr(ifStmt->condition.get());
+
+        std::cout << ", then = ";
+        printStmt(ifStmt->thenBranch.get());
+
+        if (ifStmt->elseBranch)
+        {
+            std::cout << ", else = ";
+            printStmt(ifStmt->elseBranch.get());
+        }
+
+        std::cout << ")";
+    }
+    else if (const auto* whileStmt = dynamic_cast<const WhileStmt*>(stmt))
+    {
+        std::cout << "While(";
+        printExpr(whileStmt->condition.get());
+        std::cout << ", body = ";
+        printStmt(whileStmt->body.get());
+        std::cout << ")";
+    }
+    else if (const auto* forStmt = dynamic_cast<const ForStmt*>(stmt))
+    {
+        std::cout << "For(";
+
+        std::cout << "init = ";
+        printStmt(forStmt->initializer.get());
+
+        std::cout << ", condition = ";
+        printExpr(forStmt->condition.get());
+
+        std::cout << ", increment = ";
+        printExpr(forStmt->increment.get());
+
+        std::cout << ", body = ";
+        printStmt(forStmt->body.get());
+
+        std::cout << ")";
+    }
+    else if (const auto* block = dynamic_cast<const BlockStmt*>(stmt))
+    {
+        std::cout << "Block{\n";
+
+        for (const auto& statement : block->statements)
+        {
+            std::cout << "  ";
+            printStmt(statement.get());
+            std::cout << "\n";
+        }
+
+        std::cout << "}";
+    }
+    else if (const auto* func = dynamic_cast<const FunctionStmt*>(stmt))
+    {
+        std::cout << "Function(";
+        std::cout << func->returnType.val << " ";
+        std::cout << func->name.val;
+        std::cout << "(";
+
+        for (size_t i = 0; i < func->params.size(); i++)
+        {
+            if (i > 0)
+                std::cout << ", ";
+
+            std::cout << func->params[i].val;
+        }
+
+        std::cout << ") ";
+
+        printStmt(func->body.get());
+
+        std::cout << ")";
+    }
+    else if (const auto* ret = dynamic_cast<const ReturnStmt*>(stmt))
+    {
+        std::cout << "Return(";
+
+        if (ret->value)
+        {
+            printExpr(ret->value.get());
+        }
+
+        std::cout << ")";
+    }
+    else
+    {
+        std::cout << "UnknownStmt";
+    }
+}
---
+++ src/utils/print_type.h
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "../ast/ast.h"
+
+std::string tokenTypeToString(TokenType type)
+{
+    switch (type)
+    {
+        case TokenType::KW_INT:
+            return "KW_INT";
+        case TokenType::KW_FLOAT:
+            return "KW_FLOAT";
+        case TokenType::KW_STR:
+            return "KW_STR";
+
+        case TokenType::PLUS:
+            return "PLUS";
+        case TokenType::MINUS:
+            return "MINUS";
+        case TokenType::STAR:
+            return "STAR";
+        case TokenType::SLASH:
+            return "SLASH";
+        case TokenType::ASSIGN:
+            return "ASSIGN";
+        case TokenType::GREATER:
+            return "GREATER";
+        case TokenType::LESS:
+            return "LESS";
+        case TokenType::BANG:
+            return "BANG";
+        case TokenType::EE:
+            return "EE";
+        case TokenType::NE:
+            return "NE";
+        case TokenType::GEQ:
+            return "GEQ";
+        case TokenType::LEQ:
+            return "LEQ";
+        case TokenType::PLUS_EQUAL:
+            return "PLUS_EQUAL";
+        case TokenType::MINUS_EQUAL:
+            return "MINUS_EQUAL";
+        case TokenType::PLUS_PLUS:
+            return "PLUS_PLUS";
+        case TokenType::MINUS_MINUS:
+            return "MINUS_MINUS";
+
+        case TokenType::FOR:
+            return "FOR";
+        case TokenType::WHILE:
+            return "WHILE";
+        case TokenType::ELSE:
+            return "ELSE";
+        case TokenType::IF:
+            return "IF";
+
+        case TokenType::IDENTIFIER:
+            return "IDENTIFIER";
+        case TokenType::OPEN_PAREN:
+            return "OPEN_PAREN";
+        case TokenType::CLOSE_PAREN:
+            return "CLOSE_PAREN";
+        case TokenType::OPEN_BRACE:
+            return "OPEN_BRACE";
+        case TokenType::CLOSE_BRACE:
+            return "CLOSE_BRACE";
+        case TokenType::RETURN:
+            return "RETURN";
+        case TokenType::VAR:
+            return "VAR";
+        case TokenType::SEMICOLON:
+            return "SEMICOLON";
+        case TokenType::TK_EOF:
+            return "TK_EOF";
+        case TokenType::ERROR:
+            return "ERROR";
+        // case TokenType::MAIN:
+        //     return "MAIN";
+        case TokenType::CONST:
+            return "CONST";
+
+        case TokenType::VAL_FLOAT:
+            return "VAL_FLOAT";
+        case TokenType::VAL_STRING:
+            return "VAL_STRING";
+        case TokenType::VAL_INT:
+            return "VAL_INT";
+
+        default:
+            return "UNKNOWN_TOKEN";
+    }
+}
