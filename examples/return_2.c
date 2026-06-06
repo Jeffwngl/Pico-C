5938c4b7402dc66a3983a96277326d055abe6c3c
Jeff613 <143241899+Jeffwngl@users.noreply.github.com>
2026-06-06 14:58:37 +1000

    array examples

--- examples/return_2.c
+++ examples/return_2.c
@@ -1,11 +1,34 @@
 // This is a comment.
+
+int add(int a, int b){
+    return a + b;
+}
+
 int main() {
-    int a_number = 4 - 2;
-    if (a_number == 3) {
-        return 0;
+    int f0 = 1;
+    int f1 = 1;
+    int f2 = 0;
+    for (int i = 0; i <= (123-1); i++) {
+        f2 =f0 + f1;
+        f0 = f1;
+        f1 = f2;
+    }
+
+    char hi[10] = "hi";
+
+    char* b = "hello";
+
+    int hello[5] = {1, 2, 3, 4, 5};
+
+    if (f1 == 2) {
+        f2 = 0;
+    }
+    else if (f2 == 0) {
+        f1 = 2;
     }
     else {
-        printf("hello");
+        f0 = 0;
     }
-    return a_number;
+
+    return 0;
 }
